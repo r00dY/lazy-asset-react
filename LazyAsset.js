0d8fca7577d5df92c36607d6699d167a0e437795
Andrzej Dąbrowski <andrzej@ratio.team>
2018-08-06 14:35:22 +0200

    init

---
+++ LazyAsset.js
@@ -0,0 +1,172 @@
+import React from "react";
+import VisibilitySensor from 'react-visibility-sensor';
+import PropTypes from 'prop-types';
+
+/**
+ * TODO:
+ *
+ * - videos
+ * - contain mode
+ * - IE / Edge fallbacks
+ * - onLoad callbacks
+ *
+ */
+
+const styles = {
+    LazyAsset: {
+        position: 'relative'
+    },
+    LazyAsset__Wrapper: {
+        position: "relative",
+        width: "100%",
+        height: "100%",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat"
+    },
+    LazyAsset__WrapperOverflow: {
+        position: "absolute",
+        top: "0",
+        left: "0",
+        width: "100%",
+        height: "100%",
+        overflow: "hidden"
+    },
+    img: {
+        position: "absolute",
+        top: "0",
+        left: "0",
+        width: "100%",
+        height: "100%",
+        objectFit: "cover",
+        objectPosition: "50% 50%",
+
+        // IE fallback
+        backgroundPosition: "center center",
+        backgroundSize: "cover"
+    }
+};
+
+
+class LazyAsset extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            status: 0, // 0 - nothing, 1 - staged, 2 - loading, 3 - loaded
+            visible: false
+        };
+
+        this.handleImageLoaded = this.handleImageLoaded.bind(this);
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
+    }
+
+    _getSrcset(images) {
+
+        let result = "";
+        images.forEach((image) => {
+            result += `${image.url} ${image.w}w, `;
+        });
+
+        return result;
+    }
+
+    handleImageLoaded() {
+        this.setState({
+            status: 3
+        });
+    }
+
+    handleVisibilityChange(visible) {
+        this.setState({
+            visible: visible,
+            status: this.state.status === 1 && visible ? 2 : this.state.status
+        });
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.load && this.state.status === 0) {
+
+            this.setState({
+                status: this.state.visible || !this.props.loadWhenInViewport ? 2 : 1
+            });
+
+        }
+    }
+
+    render() {
+
+        // Extra styles for LazyAsset__Wrapper
+        let extraStyles = {};
+
+        if (this.props.mode === "natural") {
+            extraStyles.paddingBottom = `${(this.props.images[0].h / this.props.images[0].w * 100)}%`;
+        }
+        if (this.props.placeholder) {
+            extraStyles.backgroundImage = `url(${this.props.placeholder})`;
+        }
+        if (this.props.backgroundColor) {
+            extraStyles.backgroundColor = this.props.backgroundColor;
+        }
+
+        return <div className={`LazyAsset ${this.props.className}`} style={{...styles.LazyAsset, ...this.props.style}}>
+
+            <VisibilitySensor onChange={this.handleVisibilityChange}>
+
+                <div className={"LazyAsset__Wrapper"} style={{...styles.LazyAsset__Wrapper, ...extraStyles}}>
+
+                    <div className={"LazyAsset__WrapperOverflow"}
+                         style={{...styles.LazyAsset__WrapperOverflow, transition: `opacity ${this.props.animationTime}s`, opacity: this.state.status === 3 ? 1 : 0}}>
+
+                        {this.props.images.length > 0 &&
+                        <img
+                            style={styles.img}
+                            sizes={this.props.sizes}
+                            alt={this.props.alt}
+                            srcSet={this.state.status >= 2 ? this._getSrcset(this.props.images) : ''}
+                            onLoad={this.handleImageLoaded}
+                        />
+                        }
+
+                    </div>
+
+                    {this.props.children}
+
+                </div>
+
+            </VisibilitySensor>
+
+        </div>
+    }
+}
+
+
+LazyAsset.propTypes = {
+    mode: PropTypes.oneOf(["cover", "natural"]).isRequired,
+    className: PropTypes.string,
+    style: PropTypes.object,
+    animationTime: PropTypes.number,
+    placeholder: PropTypes.string,
+    loadWhenInViewport: PropTypes.bool,
+    sizes: PropTypes.string,
+    alt: PropTypes.string,
+    preload: PropTypes.bool,
+    images: PropTypes.arrayOf(PropTypes.object),
+    loaded: PropTypes.bool,
+    backgroundColor: PropTypes.string,
+};
+
+LazyAsset.defaultProps = {
+    className: "",
+    style: {},
+    animationTime: 1,
+    loadWhenInViewport: false,
+    sizes: "100vw",
+    preload: false,
+    images: [],
+    loaded: false,
+    backgroundColor: "lightgrey"
+};
+
+
+export default LazyAsset;
---
+++ stories/index.stories.js
@@ -0,0 +1,189 @@
+import React from 'react';
+
+import {storiesOf} from '@storybook/react';
+
+import LazyAsset from '../LazyAsset.js';
+
+const CAT_LANDSCAPE = [
+    {
+        url: "/cat/cat_420.png",
+        w: 420,
+        h: 236
+    },
+    {
+        url: "/cat/cat_768.png",
+        w: 768,
+        h: 430
+    },
+    {
+        url: "/cat/cat_1024.png",
+        w: 1024,
+        h: 534
+    },
+    {
+        url: "/cat/cat_1280.png",
+        w: 1280,
+        h: 717
+    },
+    {
+        url: "/cat/cat_1600.png",
+        w: 1600,
+        h: 896
+    },
+    {
+        url: "/cat/cat_1920.png",
+        w: 1920,
+        h: 1075
+    }
+];
+
+const styles = {
+    imageLabel: {
+        position: "absolute",
+        top: "-10px",
+        left: "-10px",
+        height: "calc(100% + 20px)",
+        width: "calc(100% + 20px)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(255, 0, 0, 0.2)"
+    }
+};
+
+function renderAllModes(options, loaded) {
+
+    options = options || {};
+
+    return (
+        <div>
+            <h3>Natural</h3>
+            <LazyAsset
+                mode={"natural"}
+                sizes={"100vw"}
+                alt={"alternative text"}
+                images={CAT_LANDSCAPE}
+
+                style={{
+                    width: "600px",
+                }}
+
+                load={loaded}
+
+                {...options}
+            />
+            <h3>Cover</h3>
+            <LazyAsset
+                mode={"cover"}
+                sizes={"100vw"}
+                alt={"alternative text"}
+                images={CAT_LANDSCAPE}
+
+                style={{
+                    width: "600px",
+                    height: "400px"
+                }}
+
+                load={loaded}
+
+                {...options}
+            />
+        </div>
+    );
+}
+
+class LazyAssetStory__Options extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loaded: false
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+
+    }
+
+    handleClick() {
+        this.setState({
+            loaded: true
+        });
+    }
+
+    render() {
+        return <div>
+            <h2>Standard</h2>
+
+            <p>Things to test</p>
+            <ul>
+                <li>Tag should be <em>img</em></li>
+                <li><em>alt</em> tag should be present</li>
+                <li>animation should be fade</li>
+                <li>class should be added</li>
+                <li><em>sizes</em> param should be properly filled</li>
+            </ul>
+
+            <div style={{marginBottom: "16px"}}>
+                <button onClick={this.handleClick}>Load images</button>
+            </div>
+
+            {renderAllModes({
+                className: "testClassAdded"
+            }, this.state.loaded)}
+
+            <h2>Placeholder + extra markup (edges out of image) + no fade animation</h2>
+
+            <div style={{marginBottom: "16px"}}>
+                <button onClick={this.handleClick}>Load images</button>
+            </div>
+
+            {renderAllModes({
+                placeholder: "/pattern.png",
+                children: <div style={styles.imageLabel}><h1>Bang</h1></div>,
+                animationTime: 0
+            }, this.state.loaded)}
+
+        </div>
+    }
+}
+
+class LazyAssetStory__LoadOnScroll extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>
+            <h2>Lazy loading</h2>
+
+            {[...Array(30)].map((e, i) =>
+                <LazyAsset
+                    mode={"natural"}
+                    sizes={"100vw"}
+                    images={[
+                        {
+                            url: `https://placeholdit.co//i/555x${300 + i*10}`,
+                            w: 555,
+                            h: 300 + i*10
+                        }
+                    ]}
+
+                    style={{
+                        width: "400px",
+                        marginBottom: "40px"
+                    }}
+
+                    load={true}
+                    loadWhenInViewport={true}
+                />
+            )}
+
+        </div>
+    }
+}
+
+storiesOf('LazyAsset', module)
+    .add('options', () => <LazyAssetStory__Options/>)
+    .add('load on scroll', () => <LazyAssetStory__LoadOnScroll/>);
